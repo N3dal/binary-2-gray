704403767acca7478b30a5246280e581cf5021a6
N3dal <nedalxzo@gmail.com>
2022-04-10 04:48:42 +0200

    update main.py

--- main.py
+++ main.py
@@ -32,26 +32,41 @@ def clear():
 clear()
 
 
-def bin2gray(code: object = None):
-    """convert binary code to gray code."""
+def bin2gray(code: object = None, code_type: str = "b"):
+    """convert binary code to gray code.
+    code type are:
+    i => integer.
+    b => binary."""
 
     # guard conditions.
 
-    if type(code) != int:
-        try:
-            code = bin(int(code))[2:]
-        except ValueError:
+    if not code:
+        # if the object is empty or none.
+        return -1
+
+    if type(code) is str:
+        # if the object is a string, or any type else.
+        if code_type == "b":
+            if [num for num in code if num not in ('0', '1')]:
+                # if code contain non binary num for example string or int.
+                return -1
+
+        elif code_type == "i":
+            try:
+                code = bin(int(code))[2:]
+
+            except ValueError:
+                return -1
+
+        else:
             return -1
-    else:
+
+    elif type(code) is int:
         # convert the integer number to bin.
         code = bin(code)[2:]
 
-    if not code:
-        return -1
-
-    if [num for num in code if num not in ('0', '1')]:
-        # if code contain non binary num for example string or int.
-
+    else:
+        # for other types.
         return -1
 
     # if everything seems ok.
@@ -73,7 +88,7 @@ def bin2gray(code: object = None):
 
 def main():
     print(bin(403)[2:])
-    print(bin2gray("403.3"))
+    print(bin2gray("1234", "i"))
 
 
 if __name__ == "__main__":
