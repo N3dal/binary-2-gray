0a9bb64494f29a0d6bb1d5e9a8674fe0f1f0ae0a
N3dal <nedalxzo@gmail.com>
2022-04-11 00:14:43 +0200

    update gray2bin function in main.py

--- main.py
+++ main.py
@@ -86,12 +86,16 @@ def bin2gray(code: object = None, code_type: str = "b"):
     return "".join(gray_code)
 
 
-def gray2bin(code: object = None, code_type: str = "b"):
+def gray2bin(code: object = None):
     """convert gray code to bin number/code.
-    code its the bin number or the int or the bin/int as string.
-    code type are:
-    'i' => integer.
-    'b' => binary.
+    this will convert only the gray-represention of the binary,
+    numbers to the normal-binary number representation.
+    so the input must be an binary number as string.
+    and the output will be an binary number as string.
+
+
+    input => gray-code(binary-representation).
+    output => bin-code(binary-representation).
     """
 
     # guard conditions.
@@ -100,36 +104,27 @@ def gray2bin(code: object = None, code_type: str = "b"):
         # if the object is empty or none.
         return -1
 
-    if type(code) is str:
-        # if the object is a string, or any type else.
-        if code_type == "b":
-            if [num for num in code if num not in ('0', '1')]:
-                # if code contain non binary num for example string or int.
-                return -1
+    if type(code) != str:
+        # if the type isn't string then isn't bin number.
+        return -1
 
-        elif code_type == "i":
-            try:
-                code = bin(int(code))[2:]
-
-            except ValueError:
-                return -1
-
-        else:
-            return -1
-
-    elif type(code) is int:
-        # convert the integer number to bin.
-        code = bin(code)[2:]
+    if [num for num in code if num not in ('0', '1')]:
+        # if code contain non binary num for example string or int.
+        return -1
 
 
 def main():
 
     INT_NUMBER = 333
-    bin_number = bin(INT_NUMBER)[2:]
+    bin_number1 = bin(INT_NUMBER)[2:]
+    gray_number = bin2gray(bin_number1)
+    bin_number2 = gray2bin("")
 
-    print(bin_number)
-    print('#'*8)
-    print(bin2gray(bin_number, 'b'))
+    print(bin_number1)
+    print('#'*10)
+    print(gray_number)
+    print('#'*10)
+    print(bin_number2)
 
 
 if __name__ == "__main__":
